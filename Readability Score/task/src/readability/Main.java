150fc6ae5b88938fa037ae18e9852edf02ac54f8
cryptotradercheese <cryptotradercheese@gmail.com>
2021-06-27 06:58:47 +1000

    stage 2

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,16 +1,23 @@
 package readability;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
+        String[] sentences = text.split("[.!?]");
 
-        if (text.length() > 100) {
-            System.out.print("HARD");
+        int wordsCount = 0;
+        for (String sentence : sentences) {
+            wordsCount += sentence.split("(?<=\\w)\\W").length;
+        }
+
+        if ((double) wordsCount / sentences.length > 10) {
+            System.out.println("HARD");
         } else {
-            System.out.print("EASY");
+            System.out.println("EASY");
         }
     }
 }
